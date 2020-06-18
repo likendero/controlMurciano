27a4d2f0f34b4ed2c5092bdd635b814dfeb9ca90
likendero <likendero11@gmail.com>
2020-06-18 18:53:52 +0200

    los ficheros de pruebas anteriores se eleminan para que no aparezcan en la fusion con master

--- test/testArchivos.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-//import assert from 'assert';
-var show_files_1 = require("./../comands/show_files");
-var sf = new show_files_1.ShowFiles('C:\Users\liken\Desktop');
-console.log(sf);
-console.log(sf.listarDirectorio());
--- test/testArchivos.ts
+++
@@ -1,7 +0,0 @@
-//import assert from 'assert';
-import {ShowFiles} from './../comands/show_files';
-
-
-const sf = new ShowFiles('C:\Users\liken\Desktop');
-console.log(sf);
-console.log(sf.listarDirectorio());
